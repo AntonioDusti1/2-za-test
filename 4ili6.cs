719ebd34c66481b312014925af8ac84b0cff0f55
AntonioDusti1 <152974953+AntonioDusti1@users.noreply.github.com>
2024-01-23 12:54:10 +0100

    Add files via upload

---
+++ 4ili6.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace de
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Unesi broj: ");
+            int number = int.Parse(Console.ReadLine());
+
+            if (number % 4 == 0 && number % 6 == 0)
+            {
+                Console.WriteLine("Broj je 4 i 6");
+            }
+            else if (number % 4 == 0)
+            {
+                Console.WriteLine("Broj je  4");
+            }
+            else if (number % 6 == 0)
+            {
+                Console.WriteLine("Broj je  6");
+            }
+            else
+            {
+                Console.WriteLine("Broj nije visibilan 4 ili 6");
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
