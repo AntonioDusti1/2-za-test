2051f31ab1728f0163c0369b20a457cfe9398aa0
AntonioDusti1 <152974953+AntonioDusti1@users.noreply.github.com>
2024-01-23 12:54:30 +0100

    Add files via upload

---
+++ PDV.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDV
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Unesi cijenu bez PDV: ");
+            decimal bezPDV = decimal.Parse(Console.ReadLine());
+
+            Console.Write("Unesi cijenu PDV: ");
+            decimal saPDV = decimal.Parse(Console.ReadLine());
+
+            decimal PDV = saPDV  - bezPDV;
+
+            Console.WriteLine("Vrijednost PDV je: " + PDV);
+            Console.ReadLine();
+        }
+    }
+}
