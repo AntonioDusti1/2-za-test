51f86986d5b707b559c5a5a1ecf3a1c32adf7dd9
AntonioDusti1 <152974953+AntonioDusti1@users.noreply.github.com>
2024-01-23 12:33:30 +0100

    Add files via upload

---
+++ rijecu.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @string
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Upisi rijec: ");
+            string input = Console.ReadLine();
+
+            string output = input;
+
+            if (input.Length >= 5)
+            {
+                output = input.Substring(2, input.Length - 4);
+            }
+
+            Console.WriteLine("Modificiraj rijec: " + output);
+            Console.ReadLine();
+        }
+    }
+}
